cc3c28adc949c5e93f7c681aeb4f436aa6fccf02
praveen.batchu@phenompeople.com <praveen.batchu@phenompeople.com>
2025-10-03 21:22:50 +0530

    jenkins grovy

--- my_first_job.groovy
+++ my_first_job.groovy
@@ -1,7 +1,27 @@
 job('hello-world-job') {
     description('A Jenkins job that prints hello')
-    
     steps {
         shell('echo hello')
+    }
+}
+
+job('welcome-message-job') {
+    description('A Jenkins job that prints a welcome message')
+    steps {
+        shell('echo Welcome to Jenkins!')
+    }
+}
+
+job('status-check-job') {
+    description('A Jenkins job that prints system status')
+    steps {
+        shell('echo System is running')
+    }
+}
+
+job('test-environment-job') {
+    description('A Jenkins job that prints test environment info')
+    steps {
+        shell('echo Running in test environment')
     }
 }
